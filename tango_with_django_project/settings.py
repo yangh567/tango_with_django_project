dd493827ea6d44ece0851fe8f6a281d8a58e107e
zhouyang shen <2359009s@student.gla.ac.uk>
2019-01-24 20:45:51 +0000

    End of chapter3

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'olfu(v#)h0r5)+=%vl$!45cx(+%s)(pp+4_6y0kuhh-n8dqma4'
+SECRET_KEY = '#1xv4u)e8h71anau9(0+32916-sxmq$p-lqwntin%)_qbi7_#x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rango',
 ]
 
 MIDDLEWARE = [
